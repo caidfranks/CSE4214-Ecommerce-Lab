a0fba6bf8f34174d89280ad1c121eb0d2dbe6cf3
Jalon Glenn <jeg9137@gmail.com>
2025-10-06 20:20:03 -0500

    Update Program.cs

--- GameVaultWeb/Program.cs
+++ GameVaultWeb/Program.cs
@@ -1,13 +1,29 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using GameVaultWeb;
-using BlazorQuickStart.Services;
+using GameVaultWeb.Components;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<UserState>();
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
-await builder.Build().RunAsync();
+builder.Services.AddSingleton<GameVaultWeb.Services.UserState>();
+builder.Services.AddScoped<GameVaultWeb.Services.ProductService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
