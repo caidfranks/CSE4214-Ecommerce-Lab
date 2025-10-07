ef5f2db83fad9c3e2c84d42d9ab37ace9c520bfa
Jalon Glenn <jeg9137@gmail.com>
2025-10-06 20:14:33 -0500

    Update UserState.cs

--- GameVaultWeb/Services/UserState.cs
+++ GameVaultWeb/Services/UserState.cs
@@ -1,8 +1,41 @@
-namespace BlazorQuickStart.Services;
-
-public class UserState
+namespace GameVaultWeb.Services
 {
-    public bool IsLoggedIn { get; set; }
-    public string Role { get; set; } = "Customer";
-    public string? Username { get; set; }
+    public class UserState
+    {
+        private bool _isLoggedIn;
+        private string? _username;
+        private string _role = "Guest";
+
+        public bool IsLoggedIn
+        {
+            get => _isLoggedIn;
+            set
+            {
+                _isLoggedIn = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+        public string? Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+        public string Role
+        {
+            get => _role;
+            set
+            {
+                _role = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+        public event Action? StateChanged;
+    }
 }
