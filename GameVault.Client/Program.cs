deea82db2661f3cdd056c43985a2d20730428970
Jalon Glenn <jeg560@msstate.edu>
2025-10-16 15:36:31 -0500

    Browse-basics but fixed.

--- GameVault.Client/Program.cs
+++ GameVault.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GameVault.Client;
 using GameVault.Client.Services;
+using GameVaultWeb.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,5 +14,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseU
 
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ListingService>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<UserState>();
 
 await builder.Build().RunAsync();
---
+++ GameVault.Client/Services/UserState.cs
@@ -0,0 +1,41 @@
+﻿namespace GameVaultWeb.Services
+{
+    public class UserState
+    {
+        private bool _isLoggedIn;
+        private string? _username;
+        private string _role = "Guest";
+
+        public bool IsLoggedIn
+        {
+            get => _isLoggedIn;
+            set
+            {
+                _isLoggedIn = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+        public string? Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+        public string Role
+        {
+            get => _role;
+            set
+            {
+                _role = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+        public event Action? StateChanged;
+    }
+}
