99cb4eca69a0fab15671824477a1ba682b4caa9e
Mrwhit05 <76916721+Mrwhit05@users.noreply.github.com>
2025-10-23 18:59:07 -0500

    Merge from browse-basics
    Merge files for product page integration

---
+++ GameVault.Client/Pages/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace GameVaultWeb.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public string VendorName { get; set; } = "";
+        public string ThumbnailUrl { get; set; } = "";
+        public string Category { get; set; } = "";
+        public int Stock { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
